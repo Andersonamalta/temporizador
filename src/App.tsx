373e0568fe4186ca6cd1291fe0313cf257110cf9
Anderson Malta <anderson_malta16@hotmail.com>
2023-08-26 13:44:40 -0300

    Criação do componente Formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from './components/Button';
+import Formulario from './components/Forms';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Formulario/>
     </div>
   );
 }
