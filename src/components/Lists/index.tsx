1d07a5c3004da9cfed9e0fcdc14859273603124c
Anderson Malta <anderson_malta16@hotmail.com>
2023-08-26 15:45:04 -0300

    Update estilização componente Lista

--- src/components/Lists/index.tsx
+++ src/components/Lists/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './style.scss';
+import style from './List.module.scss';
 
 
 function Lista() {
@@ -16,11 +16,11 @@ function Lista() {
     ]
 
     return(
-        <aside className="listaTarefas">
+        <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, i) =>(
-                    <li key={i} className="item">
+                    <li key={i} className={style.item}>
                         <h3>{item.tarefa}</h3>
                         <span>{item.tempo}</span>
                     </li>
