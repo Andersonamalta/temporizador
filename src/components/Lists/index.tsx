87ff32e34b9c393b3348c7dbd3f6b036e8de2165
Anderson Malta <anderson_malta16@hotmail.com>
2023-09-02 15:45:25 -0300

    Controlando o formulário

--- src/components/Lists/index.tsx
+++ src/components/Lists/index.tsx
@@ -1,10 +1,11 @@
+import { useState } from 'react';
 import Item from './Item';
 import style from './List.module.scss';
 
 
 function Lista() {
 
-    const tarefas = [
+    const [tarefas, setTarefas] = useState([
         {
           tarefa: 'React',
           tempo: '02:00:00'
@@ -13,11 +14,13 @@ function Lista() {
           tarefa: 'JavaScript',
           tempo: '01:00:00'
         }
-    ]
+    ]); 
 
     return(
         <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() =>{
+                setTarefas([...tarefas, {tarefa: "State React", tempo: "05:00:00"}])
+            }}>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, i) =>(
                    <Item
